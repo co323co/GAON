5ee02dba088c0173cd89f127e46f497561e0a0e3
co323co <co323co@gmail.com>
2021-08-25 18:16:10 +0900

    init sql DB Password 설정

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -2,4 +2,4 @@
 -- create database IF NOT EXISTS ssafy_web_db collate utf8mb4_general_ci;
 create database IF NOT EXISTS gaon collate utf8mb4_general_ci;
 -- DB 비밀번호 설정
-ALTER user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dlfgofk!0!';
+ALTER user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'DLFgofk!0!';
